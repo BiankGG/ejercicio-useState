be55e82ba2a6c669d95bb24295b375fcb5aed6a4
Bianca G. Griffin <Biancagriffin@hotmail.es>
2024-06-17 21:42:31 +0200

    react new

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,25 @@
+import React, { useState } from 'react'
 import './App.css'
 
 function App() {
- //Aquí tu código
+    const [name,setName]= useState('Sophia');
+    const [newName, setNewName]= useState('');
+ 
+    const changeName =(TeacherName) => {
+        setName(TeacherName)
+    };
+
+    return (
+        <div>
+       <h2>Teachers Name :{name}</h2>
+       <ul>
+        <li onClick= {()=>changeName('Data')}>Data</li>
+        <li onClick={()=>changeName('Reyes')}>Reyes</li>
+        <li onClick={()=>changeName('Yolanda')}>Yolanda</li>
+       </ul>
+        </div>
+        
+    )
 }
 
 export default App
